3ba4f2723968812645a3cb66ccad72566841b069
Pablo Botta <pabloluisbotta@gmail.com>
2023-08-01 10:29:40 -0300

    color in editor

--- admin/src/config/customTools.js
+++ admin/src/config/customTools.js
@@ -15,6 +15,7 @@ import Delimiter from '@editorjs/delimiter'
 import InlineCode from '@editorjs/inline-code'
 import Columns from '@calumk/editorjs-columns'
 import EditorJS from '@editorjs/editorjs';
+import { ColorPlugin } from 'editorjs-text-color-plugin'
 let customToolsCol = {
   raw: Raw,
   header: Header,
@@ -22,6 +23,15 @@ let customToolsCol = {
 };
 
 let customTools = {
+  Color: {
+    class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+    config: {
+        colorCollections: ['#EC7878','#9C27B0','#673AB7','#3F51B5','#0070FF','#03A9F4','#00BCD4','#4CAF50','#8BC34A','#CDDC39', '#FFF'],
+        defaultColor: '#FF1300',
+        type: 'text', 
+        customPicker: true // add a button to allow selecting any colour  
+    }     
+  },
   embed: {
     class: Embed,
     config: {
